574a6d8f128689cdc6f648136baa8bef31e85a17
xydui <160798869+xydui@users.noreply.github.com>
2024-02-28 15:40:33 -0800

    Update 547Project.py

--- 547Project.py
+++ 547Project.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import pandas as pd
 
-st.write('Hello world!')
+
+
+# Load the dataset
+df = pd.read_excel('Combined.xlsx')
+print(df.head())
+
+
+
+
+# Streamlit Layout
+st.title('Concert Feedback Analysis')
+
+artist_selection = st.selectbox('Select an Artist', df['Artist'].unique())
+artist_reviews = df[df['Artist'] == artist_selection]['Review']
+
+
+
+st.write(artist_reviews[0:3])
