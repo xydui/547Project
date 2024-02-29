54d779c71ac5183a9440f6e4439b914753cb78fa
xydui <160798869+xydui@users.noreply.github.com>
2024-02-28 16:10:52 -0800

    Update 547Project.py

--- 547Project.py
+++ 547Project.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 # Load the dataset
-df = pd.read_csv('https://github.com/xydui/547Project/blob/main/Combined.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/xydui/547Project/main/Combined.csv')
 print(df.head())
 
 
