e06b422f1b73e7e22e80358e01e9c1293b304d4b
xydui <160798869+xydui@users.noreply.github.com>
2024-02-28 18:20:33 -0800

    Update 547Project.py

--- 547Project.py
+++ 547Project.py
@@ -1,21 +1,46 @@
 import streamlit as st
 import pandas as pd
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
 
-
-
-# Load the dataset
+# ---------------------------
+# Load Dataset
+# ---------------------------
 df = pd.read_csv('https://raw.githubusercontent.com/xydui/547Project/main/Combined.csv')
 print(df.head())
 
 
+# ---------------------------
+# Word Cloud Settings
+# ---------------------------
+custom_stopwords = STOPWORDS.union({'concert', 'show', 'see', 'one'})
+
+def generate_wordcloud(text):
+    wordcloud = WordCloud(width = 800, height = 400, background_color = 'white', stopwords = custom_stopwords).generate(text)
+    plt.figure(figsize = (10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
 
 
+# ---------------------------
 # Streamlit Layout
+# ---------------------------
+# title
 st.title('Concert Feedback Analysis')
 
+# get data from dataset
 artist_selection = st.selectbox('Select an Artist', df['Artist'].unique())
-artist_reviews = df[df['Artist'] == artist_selection]['Review']
+artist_reviews = df[df['Artist'] == artist_selection]['Review'].tolist()
+
+# word cloud
+if st.button('Show Word Cloud'):
+    combined_text = " ".join(artist_reviews)
+    st.set_option('deprecation.showPyplotGlobalUse', False)  # To hide warning
+    generate_wordcloud(combined_text)
+    st.pyplot()
 
 
 
-st.write(artist_reviews[0:3])
+
+# st.write(artist_reviews[0:3])
