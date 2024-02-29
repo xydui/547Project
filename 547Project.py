ddec7f4964aeb73f0ceb43493a1099bece48e37d
xydui <160798869+xydui@users.noreply.github.com>
2024-02-28 23:44:32 -0800

    Update 547Project.py

--- 547Project.py
+++ 547Project.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # Load Dataset
 # ---------------------------
 df = pd.read_csv('https://raw.githubusercontent.com/xydui/547Project/main/Combined.csv')
-print(df.head())
+
 
 
 # ---------------------------
@@ -19,9 +19,31 @@ custom_stopwords = STOPWORDS.union({'concert', 'show', 'see', 'one'})
 def generate_wordcloud(text):
     wordcloud = WordCloud(width = 800, height = 400, background_color = 'white', stopwords = custom_stopwords).generate(text)
     plt.figure(figsize = (10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.imshow(wordcloud, interpolation = 'bilinear')
     plt.axis("off")
     plt.show()
+
+
+
+# ---------------------------
+# OpenAI API
+# ---------------------------
+def analyze_sentiment(review_text, openai_api_key):
+    client = OpenAI(api_key = openai_api_key)
+    
+    prompt = "Analyze the sentiment of the following review and suggest an appropriate response. \
+            Please only output the response.:\nReview: " + review_text
+
+    chat_completion = client.chat.completions.create(
+        model = "gpt-3.5-turbo-0125",
+        messages = [{"role": "user", "content": prompt}],
+        max_tokens = 50,
+        temperature = 0.7
+    )
+   
+    return chat_completion.choices[0].message.content
+
+
 
 
 # ---------------------------
@@ -31,7 +53,7 @@ def generate_wordcloud(text):
 st.title('Concert Feedback Analysis')
 
 # Textbox for entering OpenAI API key
-api_key = st.text_input("Enter your OpenAI API Key:", type="password")
+openai_api_key = st.text_input("Enter your OpenAI API Key:", type = "password")
 
 
 # get data from dataset
@@ -50,32 +72,15 @@ if st.button('Show Word Cloud'):
 # Textbox for user to enter their review
 review = st.text_area("Write your review here:")
 
-# Function to analyze sentiment
-def analyze_sentiment(review_text, api_key):
-    client = OpenAI(api_key)
-    
-    chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-3.5-turbo-0125",)
-   
-    return messages['choices'][0]['message']['content']
-
-
-
 
 
 # Button to submit review
 if st.button("Submit Review"):
-    if not api_key:
+    if not openai_api_key:
         st.error("Please enter your OpenAI API key to proceed.")
     elif not review:
         st.error("Please write a review to submit.")
     else:
         # Analyze the review's sentiment and provide feedback
-        feedback = analyze_sentiment(review, st.session_state['api_key'])
+        feedback = analyze_sentiment(review, openai_api_key)
         st.success(feedback)
