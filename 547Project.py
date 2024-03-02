6d4fb2ca9e893af8774d58481a14d618c488588a
xydui <160798869+xydui@users.noreply.github.com>
2024-03-01 22:31:34 -0800

    Update 547Project.py

--- 547Project.py
+++ 547Project.py
@@ -148,8 +148,7 @@ with CustomerTab:
     review = st.text_area("Please write your review here:")
 
     # Export review to csv file for furture analysis
-    review_inputs = []
-    review_inputs.append(review)
+    review_inputs = pd.DataFrame(review)
     review_inputs.to_csv('review_input.csv')
     
     # Button to submit review
